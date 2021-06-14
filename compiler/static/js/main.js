6af7edc6d9f53abb42120e0b295a7ee365fecd0b
Damian Tabor <zettie008@gmail.com>
2021-06-14 14:56:55 +0100

    Add form for uploading ffiles and style it

--- compiler/static/js/main.js
+++ compiler/static/js/main.js
@@ -1,5 +1,25 @@
-/*
-let myCodeMirror = CodeMirror(document.body, {
-    mode: 'xml'
+const editorContainer = document.getElementById("editor");
+const uploadBtn = document.getElementById('actual-btn');
+
+
+uploadBtn.addEventListener('change', function() {
+    let files = this.files;
+    showSelectedFiles(files);
 });
-*/
+
+
+const showSelectedFiles = (files) => {
+    const form = document.querySelector('#upload-form-container form');
+    for (const file of files) {
+        let span = document.createElement('span')
+        span.textContent = file.name;
+        form.append(span);
+    }
+};
+
+
+const myCodeMirror = CodeMirror(editorContainer, {
+    mode: 'xml',
+    viewportMargin: Infinity,
+    lineNumbers: true,
+});
