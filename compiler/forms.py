11eaf1c4124a19108176b158b587466e3781b0d9
Damian Tabor <zettie008@gmail.com>
2021-06-20 20:42:57 +0100

    Change form label

--- compiler/forms.py
+++ compiler/forms.py
@@ -2,13 +2,20 @@ from django import forms
 
 
 class UploadJackFileForm(forms.Form):
+    CHOICES = [
+        ('parser', 'parser'),
+        ('compile', 'compile')
+    ]
+
     file = forms.FileField(
                 widget=forms.FileInput(
                         attrs={'hidden': True,
                                'multiple': True}
                     ),
-                label='Upload File(s)'
+                label='Upload Jack File(s)'
             )
+    action = forms.ChoiceField(choices=CHOICES)
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
