3cc746a188768b1e88b65a35414079e4c9599bee
Damian Tabor <zettie008@gmail.com>
2021-06-20 21:11:36 +0100

    Modify form

--- compiler/forms.py
+++ compiler/forms.py
@@ -2,20 +2,13 @@ from django import forms
 
 
 class UploadJackFileForm(forms.Form):
-    CHOICES = [
-        ('parser', 'parser'),
-        ('compile', 'compile')
-    ]
-
-    file = forms.FileField(
+    file_field = forms.FileField(
                 widget=forms.FileInput(
                         attrs={'hidden': True,
                                'multiple': True}
                     ),
                 label='Upload Jack File(s)'
             )
-    action = forms.ChoiceField(choices=CHOICES)
-
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
