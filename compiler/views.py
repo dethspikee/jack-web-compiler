bf057763459acd6cbd0ac665e6e05994b3f1f3f8
Damian Tabor <zettie008@gmail.com>
2021-06-22 21:21:01 +0100

    Handle multiple files

--- compiler/views.py
+++ compiler/views.py
@@ -1,13 +1,26 @@
+import json
+
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 from .forms import UploadJackFileForm
 
 
-def index(request):
+def file_uploads(request):
     if request.method == 'POST':
         print(request.FILES)
-        return
+        form = UploadJackFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            files = request.FILES.getlist('file_field')
+            response_content = ''
+            for file_ in files:
+                decoded_file = file_.read().decode('utf-8')
+                response_content += decoded_file
+            response = {
+                    'content': response_content
+            }
+        return JsonResponse(response, status=200)
     else:
         form = UploadJackFileForm()
     return render(request,
